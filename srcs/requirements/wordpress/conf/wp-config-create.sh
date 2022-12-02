1c01cba5c0468dfcea6a343499127eafba3f5d58
Neverov Iaroslav <beeguy74@gmail.com>
2022-12-02 18:48:22 +0100

    wordpress site is working but only with wp server

--- srcs/requirements/wordpress/conf/wp-config-create.sh
+++ srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -3,4 +3,8 @@ chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 wp core download --path=/var/www/wordpress --allow-root
 cd /var/www/wordpress
-tail -f
+wp config create --dbname=wordpress --dbuser=wp_user --dbpass=1234 --dbhost=my-db --allow-root
+wp core install --url=tphung.42.fr --title="tphung-wp" --admin_user=tphung --admin_password=1234 --admin_email=info@wp-cli.org --allow-root
+
+#php-fpm7 -F
+wp server --host=0.0.0.0 --port=9000 --allow-root
